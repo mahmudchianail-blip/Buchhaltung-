88720c2f9553859dd0b304bc7eac57c371bce557
mahmudchianail-blip <mahmudchianail@gmail.com>
2025-09-06 08:08:35 +0200

    fix: add UUID generator to server database

--- OPAPP Buchhaltun/filedb.js
+++ OPAPP Buchhaltun/filedb.js
@@ -151,4 +151,12 @@ class OptisparServerDB {
         const items = await this.getAll(store);
         return items.length;
     }
+
+    generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+            const r = Math.random() * 16 | 0;
+            const v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 }
